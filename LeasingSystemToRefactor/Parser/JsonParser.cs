28f26c5e3b3ad6153aeb84242e8d4b2b2e8caafd
Rafal Kaszczuk <havi1989@gmail.com>
2018-10-26 07:56:20 +0200

    Initial commit - code to refactor

---
+++ LeasingSystemToRefactor/Parser/JsonParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LeasingSystemToRefactor.Parser
+{
+    public class JsonParser
+    {
+        public string Parse<T>(T obj, JsonSerializerSettings jsonSerializerSettings)
+        {
+            return JsonConvert.SerializeObject(obj, jsonSerializerSettings);
+        }
+    }
+}
