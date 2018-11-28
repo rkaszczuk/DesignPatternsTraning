321f6faa6d8628c2279d93860b8ed5f985dd7055
Rafal Kaszczuk <havi1989@gmail.com>
2018-11-28 13:33:10 +0100

    Fix

--- LeasingSystemToRefactor/Finance/ILeasingPaymentsCalculator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace LeasingSystemToRefactor.Finance
-{
-    public interface ILeasingPaymentsCalculator
-    {
-        List<decimal> Calculate();
-    }
-}
